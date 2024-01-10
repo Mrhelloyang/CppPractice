c1131b88358f891b1c951c450dd2bf6fb7963470
Mrhelloyang <2131424889@qq.com>
2024-01-10 16:53:40 +0800

    202401010第二次提交

--- 071_this指针的使用.cpp
+++ 071_this指针的使用.cpp
@@ -1,8 +1,9 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
+//this是隐含在每一个非静态成员函数里面的一种指针
+// 不用定义，直接使用，this指向被调用函数所属的对象
 //1.解决名称冲突
-//this指向被调用函数所属的对象；
 class Person {
 
 public:
