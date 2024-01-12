2b1e439941de6c9add67f7c6e06db0ff3fdd7f0b
Mrhelloyang <2131424889@qq.com>
2024-01-12 10:53:43 +0800

    20240112第一提交

---
+++ 079_递增运算符重载.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+class MyInteger {
+	friend ostream& operator<<(ostream& out, MyInteger myint);
+public:
+	MyInteger()
+	{
+		m_Num = 0;
+	}
+	//前置++，先++，再返回；
+	MyInteger& operator++()
+	{
+		m_Num++;
+		return *this;
+	}
+	//后置++，先用一个临时变量保存，再++，最后再返回临时变量，
+	MyInteger operator++(int)
+	{
+		MyInteger temp = *this;
+		m_Num++;
+		return temp;
+	}
+
+private:
+	int m_Num;
+};
+//这句话为什么后面用值传递不用引用，因为后置++返回值，
+ostream& operator<<(ostream& out, MyInteger myint)
+{
+	out << myint.m_Num;
+	return out;
+}
+void test01()
+{
+	MyInteger myint;
+	cout << myint << endl;
+	cout << ++myint << endl;
+	cout << myint++ << endl;
+}
+
+int test02()
+{
+	int a = 10;
+	cout << &a << endl;
+	return a;
+}
+int main()
+{
+	test01();
+	int a = test02();
+	cout << test02() << endl;
+	//cout << a << endl;
+	return 0;
+}
---
+++ 080_赋值运算符重载.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+class Person {
+
+	friend void test();
+public:
+	Person(int age)
+	{
+		this->m_age = new int(age);
+	}
+	/*Person(Person& P)
+	{
+		this->m_age = new int(*P.m_age);
+	}*/
+	Person& operator=(Person& P)
+	{
+		this->m_age = new int(*P.m_age);
+		return *this;
+	}
+	~Person()
+	{
+		//编译器会提供一个浅拷贝的拷贝构造函数，写析构函数的话可能会出现，内存重复释放的问题，用深拷贝就可以解决
+		delete m_age;
+		m_age = NULL;
+	}
+private:
+	int* m_age;
+};
+void test()
+{
+	Person P1(10);
+	cout << "P1的年龄：" << *P1.m_age << endl;
+	Person P2(20);
+	cout << "P2的年龄：" << *P2.m_age << endl;
+	Person P3 = P2;
+	cout << "P3的年龄：" << *P3.m_age << endl;
+	P3 = P2 = P1;//符号函数重载返回引用的话就可以继续执行;
+	cout << "P1的年龄：" << *P1.m_age << endl;
+	cout << "P2的年龄：" << *P2.m_age << endl;
+	cout << "P3的年龄：" << *P2.m_age << endl;
+}
+int main()
+{
+	test();
+	system("pause");
+	return 0;
+}
