995497dc56bbdc689c812aa7b99486087be1dc7f
Mrhelloyang <2131424889@qq.com>
2024-01-16 14:32:01 +0800

    20240116第二次提交

---
+++ 096_二进制文件写文件.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+#include<string>
+#include<fstream>
+class Person {
+public:
+	char m_Name[40];
+	int m_Age;
+};
+void test01()
+{
+	Person P{ "张三",18 };
+	ofstream ofs;
+	ofs.open("Person.txt", ios::out | ios::binary);
+	ofs.write((const char*)&P, sizeof(P));
+	ofs.close();
+}
+int main()
+{
+	test01();
+
+	return 0;
+}
---
+++ 097_二进制文件读文件.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+#include <fstream>
+class Person
+{
+public:
+	char m_Name[40];
+	int m_Age;
+};
+void
+test01()
+{
+	ifstream ifs("Person.txt", ios::in | ios::binary);
+	if (!ifs.is_open())
+	{
+		cout << "文件打开失败" << endl;
+		return;
+	}
+	Person P;
+	ifs.read((char*)&P, sizeof(P));
+	cout << "姓名：" << P.m_Name << "年龄：" << P.m_Age << endl;
+
+}
+int main()
+{
+	test01();
+
+	return 0;
+}
