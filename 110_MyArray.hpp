0765d3674e92b79ca9112e258d87aa8e7003378b
Mrhelloyang <2131424889@qq.com>
2024-01-24 16:14:26 +0800

    20240124第二次提交

---
+++ 110_MyArray.hpp
@@ -0,0 +1,101 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+#include<string>
+template<class T>
+class MyArray {
+public:
+	//有参构造函数
+	MyArray(int capacity)
+	{
+		this->capacity = capacity;
+		this->size = 0;
+		this->Paddress = new T[this->capacity];
+	}
+	//拷贝构造函数，为了解决浅拷贝的问题
+	MyArray(MyArray& ma)
+	{
+		this->capacity = ma.capacity;
+		this->size = ma.size;
+		this->Paddress = new T[ma.size];
+		for (int i; i < ma.size; i++)
+		{
+
+			this->Paddress[i] = ma.Paddress[i];
+		}
+	}
+	//重载=
+	MyArray& operator=(MyArray& ma)
+	{
+		if (this->capacity != NULL)
+		{
+			this->capacity = 0;
+			this->size = 0;
+			delete[] this->Paddress;
+			this->arr = NULL;
+		}
+		this->capacity = ma.capacity;
+		this->size = ma.size;
+		this->Paddress = new T[ma.size];
+		for (int i; i < ma.size; i++)
+		{
+
+			this->Paddress[i] = ma.Paddress[i];
+		}
+		return *this;
+
+	}
+	//通过下标访问，重载[]
+	T& operator[](int index)
+	{
+		if (index < 0 || index >= this->size)
+		{
+			exit(0);
+		}
+		return this->Paddress[index];
+	}
+	//尾插
+	void PushBack(T val)
+	{
+		//判断是否满了
+		if (this->capacity == this->size)
+		{
+			return;
+		}
+		this->Paddress[this->size] = val;
+		this->size++;
+	}
+	//尾删
+	void PopBack()
+	{
+		//判断是否是空的
+		if (this->size == 0)
+		{
+			return;
+		}
+		this->size--;
+	}
+	int Getsize()
+	{
+		return this->size;
+	}
+	int Getcapacity()
+	{
+		return this->capacity;
+	}
+	~MyArray()
+	{
+		if (this->Paddress != NULL)
+		{
+			delete[]this->Paddress;
+			this->Paddress = NULL;
+			this->size = 0;
+			this->capacity = 0;
+
+		}
+	}
+private:
+	T* Paddress; //指向数组
+	int capacity; //容量
+	int size;  //数组大小
+};
---
+++ 110_通用的数组类的实现.cpp
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+#include"110_MyArray.hpp"
+//测试内置数据类型
+void printIntArray(MyArray<int>& arr)
+{
+	for (int i = 0; i < arr.Getsize(); i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+void test01()
+{
+	MyArray<int> arr(10);
+	for (int i = 0; i < 10; i++)
+	{
+		arr.PushBack(i);
+	}
+	cout << "array1打印输出：" << endl;
+	printIntArray(arr);
+	cout << "array1的大小：" << arr.Getsize() << endl;
+	cout << "array1的容量：" << arr.Getcapacity() << endl;
+
+	cout << "--------------------------" << endl;
+}
+
+//测试自定义数据类型
+class Person {
+public:
+	Person() {}
+	Person(string name, int age) {
+		this->name = name;
+		this->age = age;
+	}
+public:
+	string name;
+	int age;
+};
+
+void printPersonArray(MyArray<Person>& personArr)
+{
+	for (int i = 0; i < personArr.Getsize(); i++) {
+		cout << "姓名：" << personArr[i].name << " 年龄： " << personArr[i].age << endl;
+	}
+}
+
+void test02()
+{
+	//创建数组
+	MyArray<Person> pArray(10);
+	Person p1("孙悟空", 30);
+	Person p2("韩信", 20);
+	Person p3("妲己", 18);
+	Person p4("王昭君", 15);
+	Person p5("赵云", 24);
+
+	//插入数据
+	pArray.PushBack(p1);
+	pArray.PushBack(p2);
+	pArray.PushBack(p3);
+	pArray.PushBack(p4);
+	pArray.PushBack(p5);
+
+	printPersonArray(pArray);
+
+	cout << "pArray的大小：" << pArray.Getsize() << endl;
+	cout << "pArray的容量：" << pArray.Getcapacity() << endl;
+
+}
+
+int main()
+{
+	test01();
+	test02();
+}
